cd150e7edcf469181a86fcd4d1d32ae62df87006
Terje Engelbertsen <terje.engelbertsen@aliaro.com>
2017-08-20 10:48:06 +0200

    ASP .NET Core 2.0 Fix for EF?

--- Program.cs
+++ Program.cs
@@ -21,4 +21,28 @@ namespace NetCoreWithMosh
             host.Run();
         }
     }
+
+    // For ASP .NET Core 2.0
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        var host = BuildWebHost(args);
+
+    //        host.Run();
+    //    }
+
+    //    // Tools will use this to get application services
+    //    public static IWebHost BuildWebHost(string[] args) =>
+    //        new WebHostBuilder()
+    //            .UseKestrel()
+    //            .UseContentRoot(Directory.GetCurrentDirectory())
+    //            .UseIISIntegration()
+    //            .UseStartup<Startup>()
+    //            .Build();
+    //}
 }
+
+
+
+
